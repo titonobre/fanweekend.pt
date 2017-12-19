98d72f8079ec62d5868892142540f154d5156058
titonobre <titonobre@gmail.com>
2017-12-19 01:59:47 +0000

    Add ComingSoon screen

---
+++ components/coming-soon/ComingSoon.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="content">
+    <img class="image" src="./circlelogo.png">
+    <h1 class="title">
+      Coming Soon!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  min-width: 100vw;
+  min-height: 100vh;
+  background: url(./bg_1d.png) #458db6;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 80px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.image {
+  max-width: 50vw;
+  max-height: 50vh;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,27 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'fanweekend.pt',
+    title: 'Paredes de Coura Fan Weekend',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/gif', href: '/favicon.gif' }
     ]
   },
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+
+  /**
+   * Modules
+   */
+  modules: [
+    ['@nuxtjs/google-analytics', { id: 'UA-1495990-6' }]
+  ],
   /*
   ** Build configuration
   */
