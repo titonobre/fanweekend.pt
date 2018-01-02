fdef48ddb5c9f19910361bbca758fa561707a063
Tito Nobre <titonobre@gmail.com>
2018-01-02 18:43:21 +0000

    Fix intro typo

--- components/intro/Intro.vue
+++ components/intro/Intro.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="container content has-text-centered">
       <h1>Welcome back to the Paredes de Coura Fan Weekend!</h1>
-      <p>We are very excited to announce that the second edition of the Paredes de Coura Fan Weekend is coming soon.</p>
+      <p>We are very excited to announce that the third edition of the Paredes de Coura Fan Weekend is coming soon.</p>
       <p>We will once again turn the cozy little village of Paredes de Coura in the center stage of the AFOL community. Hosted by the Comunidade 0937 we hope to gather LEGO fans from all around the world and make this an unforgettable experience with passionate AFOLs, fantastic models and creations and insightful workshops!</p>
       <p>If you've been there last year, you know what you can expect. If you haven't, you don't know what you've been missing. We have a few surprises up our sleeves for this year.</p>
       <p>Registrations will be from 1 of January to 28 of February, 2018.</p>
