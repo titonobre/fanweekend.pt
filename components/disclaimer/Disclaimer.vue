b0db06eb3ceed46154652b10f7e40b08ebfcb70a
titonobre <titonobre@gmail.com>
2018-01-02 23:21:26 +0000

    Made with ❤

--- components/disclaimer/Disclaimer.vue
+++ components/disclaimer/Disclaimer.vue
@@ -3,9 +3,7 @@
     <div class="container">
       <div class="content has-text-centered">
         <p>
-          Website maintained by
-          <a target="_blank" href="https://comunidade0937.com">Comunidade 0937</a>
-          .
+          Made with <span class="icon is-small has-text-danger"><i class="fa fa-heart" alt="love" aria-valuetext="love" title="love"></i></span> by <a target="_blank" href="https://comunidade0937.com">Comunidade 0937</a>.
         </p>
       </div>
     </div>
