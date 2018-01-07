51d6c7a70d6fb2c04297aa9f7bdd5e23758798f6
titonobre <titonobre@gmail.com>
2018-01-07 03:00:30 +0000

    Add countries to /who-is-going

--- pages/who-is-going.vue
+++ pages/who-is-going.vue
@@ -7,8 +7,15 @@
 
         <h1>Who is Going?</h1>
         <p class="is-size-7 has-text-grey-light">
-          {{ entries.length }} people from {{ countryNames.length}} countries
+          {{ entries.length }} people from {{ countries.length}} countries
         </p>
+
+        <div class="countries">
+          <div class="country" v-for="({countryCode, countryName, count}) in countries">
+            <img class="flag" :src="flags[countryCode]"  v-bind:alt="countryCode" v-bind:title="countryName" />
+            <span class="is-size-7 has-text-grey-light">{{ count }}</span>
+          </div>
+        </div>
 
         <template v-for="entry in entries">
           <h2 class="participant-name">{{ entry.name }}</h2>
@@ -20,13 +27,40 @@
       </div>
     </section>
 
-    <disclaimer/>
+    <disclaimer>
+      Flag icons provided by <a target="_blank" rel="noopener" href="https://www.flaticon.com/packs/countrys-flags/6">Freepik</a>
+    </disclaimer>
   </section>
 </template>
 
 <script>
 import Navigation from '~/components/navigation/Navigation.vue'
 import Disclaimer from '~/components/disclaimer/Disclaimer.vue'
+
+const flags = {
+  ar: require('~/assets/flags/argentina.svg'),
+  au: require('~/assets/flags/australia.svg'),
+  be: require('~/assets/flags/belgium.svg'),
+  br: require('~/assets/flags/brazil.svg'),
+  ca: require('~/assets/flags/canada.svg'),
+  cv: require('~/assets/flags/cape-verde.svg'),
+  hr: require('~/assets/flags/croatia.svg'),
+  dk: require('~/assets/flags/denmark.svg'),
+  fr: require('~/assets/flags/france.svg'),
+  de: require('~/assets/flags/germany.svg'),
+  gr: require('~/assets/flags/greece.svg'),
+  ie: require('~/assets/flags/ireland.svg'),
+  it: require('~/assets/flags/italy.svg'),
+  nl: require('~/assets/flags/netherlands.svg'),
+  no: require('~/assets/flags/norway.svg'),
+  pt: require('~/assets/flags/portugal.svg'),
+  ro: require('~/assets/flags/romania.svg'),
+  rs: require('~/assets/flags/serbia.svg'),
+  si: require('~/assets/flags/slovenia.svg'),
+  es: require('~/assets/flags/spain.svg'),
+  gb: require('~/assets/flags/united-kingdom.svg'),
+  us: require('~/assets/flags/united-states-of-america.svg')
+}
 
 function parseFeed (feed) {
   const rows = []
@@ -52,7 +86,11 @@ export default {
     Disclaimer
   },
   data () {
-    return { entries: [], countryNames: [] }
+    return {
+      entries: [],
+      counties: [],
+      flags
+    }
   },
   async asyncData ({ app, params }) {
     const data = await app.$axios.$get(url)
@@ -62,18 +100,22 @@ export default {
     const entries = rows.map(row => ({
       name: row[0],
       countryName: row[1],
-      countryCode: row[2],
+      countryCode: row[2].toLowerCase(),
       lug: row[3]
     }))
 
-    const countries = entries.reduce((acc, entry) => {
-      acc[entry.countryCode] = entry.countryName
+    const countriesMap = entries.reduce((acc, { countryCode, countryName }) => {
+      const country = acc[countryCode] = acc[countryCode] || { countryCode, countryName, count: 0 }
+
+      country.count++
+
       return acc
     }, {})
 
-    const countryNames = Object.values(countries)
+    const countries = Object.values(countriesMap)
+      .sort((a, b) => a.countryName.localeCompare(b.countryName))
 
-    return { entries, countryNames }
+    return { entries, countries }
   },
   fetch ({ app, params }) {
     // app.$axios.$get(url).then(data => console.log(data))
@@ -83,16 +125,23 @@ export default {
 
 <style scoped>
 .container {
-      padding-top: 3.25rem;
+    padding-top: 3.25rem;
 }
 
-.image {
-  max-width: 25vw;
-  max-height: 25vh;
-}
-
-.tags {
+.countries {
+  display: flex;
+  flex-wrap: wrap;
   justify-content: center;
+}
+
+.countries .country {
+  display: flex;
+  flex-direction: column;
+  padding: 0.3em
+}
+
+.countries .flag {
+  height: 1.5em;
 }
 
 .participant-name {
