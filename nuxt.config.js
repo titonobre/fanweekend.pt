34d5b594712e80586845f7d8cac5a952a4eca23f
titonobre <titonobre@gmail.com>
2018-02-09 00:44:47 +0000

    Update description meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Paredes de Coura Fan Weekend 2018' }
     ],
     link: [
       { rel: 'icon', type: 'image/gif', href: '/favicon.gif' }
