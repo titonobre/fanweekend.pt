7db5617ce5c32bd98895138ec0bd8fc00684c244
Tito Nobre <titonobre@gmail.com>
2018-03-02 13:38:10 +0000

    Remove registration form button

--- components/cover/Cover.vue
+++ components/cover/Cover.vue
@@ -3,12 +3,6 @@
     <div></div>
     <img class="image" src="../coming-soon/circlelogo.png">
     <div class="bottom ">
-      <p class="has-text-centered">
-        <a class="button is-success" target="_blank" rel="noopener" href="https://goo.gl/forms/2O4Z4dUYdrGEgKxi2">
-          <span>Just take me to the registration form</span>
-          <span class="icon"><i class="fa fa-fw fa-pencil-square-o"></i></span>
-        </a>
-      </p>
       <p class="has-text-centered">Scroll down for more details...</p>
     </div>
   </div>
