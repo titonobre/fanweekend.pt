e7e241e65b8147c55fc699952ab3f4c54cde1d60
titonobre <titonobre@users.noreply.github.com>
2021-04-06 16:43:35 +0100

    update event dates

--- components/when/When.vue
+++ components/when/When.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="container content">
       <h1>When</h1>
-      <p>The event takes place on 12 and 13 of June, 2021.</p>
+      <p>The event takes place on 19 and 20 of June, 2021.</p>
     </div>
   </section>
 </template>
