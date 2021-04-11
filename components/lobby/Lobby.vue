605d42663cc034bb2512145cee4f390bc95983e5
Tito Nobre <titonobre@gmail.com>
2021-04-11 15:47:34 +0100

    update lobby

--- components/lobby/Lobby.vue
+++ components/lobby/Lobby.vue
@@ -2,11 +2,24 @@
   <section class="section">
     <div class="container content">
       <h1>Lobby</h1>
-      <h2>Latest Announcements</h2>
 
-      <p>You can read the lastest announcements on Discord. More on that soon!</p>
+      <p>
+        Join our
+        <a target="_blank" rel="noopener" href="https://discord.gg/CTtYfD2S6Y">
+          Discord server
+        </a>
+        to get all the updates on the event. Discord is also the best way to get
+        in touch with us. Think of it as the replacement for the front desk that
+        we used to have in the exibition hall in Paredes de Coura.
+      </p>
+      <p>
+        Discord can be used in your browser, or installed on your computer or
+        smartphone. You choose.
+      </p>
       <p class="has-text-centered">
-        <img class="image" src="./discord-logo.svg" />
+        <a target="_blank" rel="noopener" href="https://discord.gg/CTtYfD2S6Y">
+          <img class="image" src="./discord-logo.svg" />
+        </a>
       </p>
     </div>
   </section>
