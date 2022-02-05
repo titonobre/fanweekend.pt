448c104257acb1496a707ac17015e363f1f31d5e
Tito Nobre <titonobre@gmail.com>
2022-02-05 21:49:18 +0000

    remove old version

--- components/when/When.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="section">
-    <div class="container content">
-      <h1>When</h1>
-      <p>The event takes place on June 19 and 20, 2021.</p>
-    </div>
-  </section>
-</template>
