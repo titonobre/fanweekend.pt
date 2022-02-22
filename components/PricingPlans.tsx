1cc122dac5fcef9e5c54d95dae67c57588d6ad65
Tito Nobre <titonobre@gmail.com>
2022-02-22 23:40:45 +0000

    feat: update pricing plans

--- components/PricingPlans.tsx
+++ components/PricingPlans.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
-import { Box, Stack, HStack, Text, VStack, useColorModeValue, List, ListItem, ListIcon, Button } from "@chakra-ui/react";
+import NextLink from "next/link";
+
+import { Box, Stack, HStack, Text, VStack, useColorModeValue, List, ListItem, ListIcon, Button, Link } from "@chakra-ui/react";
 import { CheckIcon } from "@chakra-ui/icons";
+import { REGISTRATION_ENABLED } from "../lib/env";
+
+const registrationEnabled = REGISTRATION_ENABLED;
 
 function PriceWrapper({ children }: { children: ReactNode }) {
   return (
@@ -20,6 +25,57 @@ export default function PricingPlans() {
       spacing={{ base: 4, lg: 10 }}
       marginY={10}
     >
+      <PriceWrapper>
+        <Box py={4} px={12}>
+          <Text fontWeight="500" fontSize="2xl">
+            Basic
+          </Text>
+          <HStack justifyContent="center">
+            <Text fontSize="3xl" fontWeight="600">
+              €
+            </Text>
+            <Text fontSize="5xl" fontWeight="900">
+              120
+            </Text>
+            <Text fontSize="3xl" color="gray.500">
+              /person
+            </Text>
+          </HStack>
+        </Box>
+        <VStack bg={useColorModeValue("gray.50", "gray.700")} py={4} roundedBottom="md">
+          <List spacing={3} textAlign="start" px={12} alignSelf="flex-start">
+            <ListItem>
+              <ListIcon as={CheckIcon} color="green.500" />
+              Accommodation
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckIcon} color="green.500" />
+              Meals
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckIcon} color="green.500" />
+              Activities
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckIcon} color="green.500" />
+              Swag
+            </ListItem>
+          </List>
+          <Box w="80%" pt={7}>
+            {registrationEnabled ? (
+              <NextLink href="/register?pack=basic" passHref>
+                <Button as={Link} w="full" colorScheme="green" variant="outline">
+                  Register
+                </Button>
+              </NextLink>
+            ) : (
+              <Button w="full" colorScheme="green" variant="outline" disabled>
+                Register
+              </Button>
+            )}
+          </Box>
+        </VStack>
+      </PriceWrapper>
       <PriceWrapper>
         <Box position="relative">
           <Box position="absolute" top="-16px" left="50%" style={{ transform: "translate(-50%)" }}>
@@ -45,7 +101,7 @@ export default function PricingPlans() {
                 €
               </Text>
               <Text fontSize="5xl" fontWeight="900">
-                125
+                150
               </Text>
               <Text fontSize="3xl" color="gray.500">
                 /person
@@ -76,55 +132,20 @@ export default function PricingPlans() {
               </ListItem>
             </List>
             <Box w="80%" pt={7}>
-              <Button w="full" colorScheme="green" disabled>
-                Register
-              </Button>
+              {registrationEnabled ? (
+                <NextLink href="/register?pack=full" passHref>
+                  <Button as={Link} w="full" colorScheme="green">
+                    Register
+                  </Button>
+                </NextLink>
+              ) : (
+                <Button w="full" colorScheme="green" disabled>
+                  Register
+                </Button>
+              )}
             </Box>
           </VStack>
         </Box>
-      </PriceWrapper>
-      <PriceWrapper>
-        <Box py={4} px={12}>
-          <Text fontWeight="500" fontSize="2xl">
-            Basic
-          </Text>
-          <HStack justifyContent="center">
-            <Text fontSize="3xl" fontWeight="600">
-              €
-            </Text>
-            <Text fontSize="5xl" fontWeight="900">
-              100
-            </Text>
-            <Text fontSize="3xl" color="gray.500">
-              /person
-            </Text>
-          </HStack>
-        </Box>
-        <VStack bg={useColorModeValue("gray.50", "gray.700")} py={4} roundedBottom="md">
-          <List spacing={3} textAlign="start" px={12} alignSelf="flex-start">
-            <ListItem>
-              <ListIcon as={CheckIcon} color="green.500" />
-              Accommodation
-            </ListItem>
-            <ListItem>
-              <ListIcon as={CheckIcon} color="green.500" />
-              Meals
-            </ListItem>
-            <ListItem>
-              <ListIcon as={CheckIcon} color="green.500" />
-              Activities
-            </ListItem>
-            <ListItem>
-              <ListIcon as={CheckIcon} color="green.500" />
-              Swag
-            </ListItem>
-          </List>
-          <Box w="80%" pt={7}>
-            <Button w="full" colorScheme="green" variant="outline" disabled>
-              Register
-            </Button>
-          </Box>
-        </VStack>
       </PriceWrapper>
     </Stack>
   );
--- lib/env.ts
+++ lib/env.ts
@@ -1,2 +1,3 @@
 export const GOOGLE_SITE_VERIFICATION_CODE: string = process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION_CODE as string;
 export const GA_TRACKING_ID: string = process.env.NEXT_PUBLIC_ANALYTICS_ID as string;
+export const REGISTRATION_ENABLED: boolean = process.env.NEXT_PUBLIC_REGISTRATION_ENABLED === "true";
