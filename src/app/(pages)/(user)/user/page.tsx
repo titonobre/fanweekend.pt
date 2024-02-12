ce1a054c12cdde2d827118d367ca90ed2fec4903
Tito Nobre <titonobre@gmail.com>
2024-02-12 23:40:48 +0000

    feat: add back button to account management page

--- src/app/(pages)/(user)/user/page.tsx
+++ src/app/(pages)/(user)/user/page.tsx
@@ -1,9 +1,18 @@
 import { UserProfile } from "@clerk/nextjs";
+import NextLink from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <section className="thin-container mt-10 flex min-h-screen w-full flex-1 flex-col items-center gap-10">
       <UserProfile />
+
+      <div className="flex justify-center">
+        <Button asChild>
+          <NextLink href="/dashboard">Back to Dashboard</NextLink>
+        </Button>
+      </div>
     </section>
   );
 }
