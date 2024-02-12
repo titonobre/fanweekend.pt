1170752ec41ef3bc1525945554014c838d58a9ee
Tito Nobre <titonobre@gmail.com>
2024-02-12 23:23:04 +0000

    feat: set intro background as fixed

--- src/components/section/intro.tsx
+++ src/components/section/intro.tsx
@@ -8,7 +8,7 @@ const logoAsset = logo as StaticImageData;
 export function Intro() {
   return (
     <div
-      className="absolute top-0 flex h-screen w-screen flex-col items-center justify-end bg-cover bg-center bg-no-repeat pb-40 sm:pb-32"
+      className="absolute top-0 flex h-screen w-screen flex-col items-center justify-end bg-cover bg-fixed bg-center bg-no-repeat pb-40 sm:pb-32"
       style={{ backgroundImage: `url('${background.src}')` }}
     >
       <div className="relative aspect-square w-52 sm:w-64 md:w-72">
