2afdf4c6ebcef67caeb509598de5cf41ecf3959e
Tito Nobre <titonobre@gmail.com>
2024-02-13 02:21:11 +0000

    feat: update google maps link

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,6 +3,6 @@ export const siteConfig = {
     comunidade0937: "https://comunidade0937.com",
     facebook: "https://www.facebook.com/groups/fanweekend.pt/",
     instagram: "https://www.instagram.com/pdcfanweekend/",
-    location: "https://goo.gl/maps/gZ8RhfG381J2",
+    location: "https://maps.app.goo.gl/kwbMpXEKS8fPPJp78",
   },
 } as const;
