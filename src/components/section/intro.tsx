a6d7a0b70221b9e1b875ac3bbce6054f03f0d62f
Tito Nobre <titonobre@gmail.com>
2025-02-03 00:13:20 +0000

    feat: update intro background

--- src/components/section/intro.tsx
+++ src/components/section/intro.tsx
@@ -1,6 +1,6 @@
 import Image, { type StaticImageData } from "next/image";
 
-import background from "~/assets/images/background.webp";
+import background from "~/assets/images/background.svg";
 import logo from "~/assets/images/pdcfw.svg";
 
 const logoAsset = logo as StaticImageData;
@@ -8,7 +8,7 @@ const logoAsset = logo as StaticImageData;
 export function Intro() {
   return (
     <div
-      className="absolute top-0 flex h-screen w-screen flex-col items-center justify-end bg-cover bg-fixed bg-center bg-no-repeat pb-40 sm:pb-32"
+      className="absolute top-0 flex h-screen w-screen flex-col items-center justify-center bg-cover bg-fixed bg-center bg-no-repeat"
       style={{ backgroundImage: `url('${background.src}')` }}
     >
       <div className="relative aspect-square w-52 sm:w-64 md:w-72">
